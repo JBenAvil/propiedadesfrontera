5059dd0f450cb254964fc7090125ac8ecd8f8a0f
JBenAvil <j.ben.avil.contacto@gmail.com>
2024-12-29 00:22:22 -0300

    add_corrects

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
   function sendWhatsAppMessage(urlImagen, titulo, detalle, precio, ) {
     // Construye el mensaje con la información de la tarjeta y el enlace de la imagen
-    const mensaje = `${urlImagen}\nHola, estoy interesado en la Propiedad: ${titulo} \nMás detalles: ${detalle} \nPrecio: ${precio}`;
+    const mensaje = `${urlImagen}\nHola, estoy interesado en la Propiedad: ${titulo} \nPrecio: ${precio}`;
 
     // Codifica el mensaje para usarlo en el enlace de WhatsApp
     const mensajeCodificado = encodeURIComponent(mensaje);
